83465631f9558cd2cc3e73485a22c46609be2c20
Hannan-Afif <hannanafifd@gmail.com>
2026-02-23 13:54:53 +0700

    Updated sonarqube version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "id.ac.ui.cs.advprog"
