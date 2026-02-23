a65a77d72dd5dfb38b30e0ec6b8eb89a5b2ceef4
Hannan-Afif <hannanafifd@gmail.com>
2026-02-23 15:35:47 +0700

    Fix 'Removing unused import UUID'

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.UUID;
+
 class ProductTest {
     Product product;
     @BeforeEach
