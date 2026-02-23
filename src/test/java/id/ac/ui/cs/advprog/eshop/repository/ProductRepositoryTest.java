0fcc895c6e7ed33f86473faf46972cc0d7aa729b
Hannan-Afif <hannanafifd@gmail.com>
2026-02-23 15:45:56 +0700

    Fix 'reduce duplication in ProductRepository tests'

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -17,24 +17,32 @@ class ProductRepositoryTest{
     @InjectMocks
     ProductRepository productRepository;
 
+    Product product1;
+    Product product2;
+
     @BeforeEach
     void setUp(){
+        product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(100);
+
+        product2 = new Product();
+        product2.setProductId("a0f9de46-90b1-437d-a0bf-d0821dde9096");
+        product2.setProductName("Sampo Cap Usep");
+        product2.setProductQuantity(50);
     }
 
     @Test
     void testCreateAndFind(){
-        Product product = new Product();
-        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        product.setProductName("Sampo Cap Bambang");
-        product.setProductQuantity(100);
-        productRepository.create(product);
+        productRepository.create(product1);
 
         Iterator<Product> productIterator = productRepository.findAll();
         assertTrue(productIterator.hasNext());
         Product savedProduct = productIterator.next();
-        assertEquals(product.getProductId(), savedProduct.getProductId());
-        assertEquals(product.getProductName(), savedProduct.getProductName());
-        assertEquals(product.getProductQuantity(), savedProduct.getProductQuantity());
+        assertEquals(product1.getProductId(), savedProduct.getProductId());
+        assertEquals(product1.getProductName(), savedProduct.getProductName());
+        assertEquals(product1.getProductQuantity(), savedProduct.getProductQuantity());
     }
     @Test
     void testFindAllIfEmpty(){
@@ -44,22 +52,15 @@ class ProductRepositoryTest{
 
     @Test
     void testFindAllIfMoreThanOneProduct(){
-        Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        product1.setProductName("Sampo Cap Bambang");
-        product1.setProductQuantity(100);
         productRepository.create(product1);
-
-        Product product2 = new Product();
-        product2.setProductId("a0f9de46-90b1-437d-a0bf-d0821dde9096");
-        product2.setProductName("Sampo Cap Usep");
-        product2.setProductQuantity(50);
         productRepository.create(product2);
 
         Iterator<Product> productIterator = productRepository.findAll();
         assertTrue(productIterator.hasNext());
+
         Product savedProduct = productIterator.next();
         assertEquals(product1.getProductId(), savedProduct.getProductId());
+
         savedProduct = productIterator.next();
         assertEquals(product2.getProductId(), savedProduct.getProductId());
         assertFalse(productIterator.hasNext());
@@ -67,11 +68,7 @@ class ProductRepositoryTest{
 
     @Test
     void testEditProduct(){
-        Product product = new Product();
-        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        product.setProductName("Sampo Cap Bambang");
-        product.setProductQuantity(100);
-        productRepository.create(product);
+        productRepository.create(product1);
 
         Product updatedProduct = new Product();
         updatedProduct.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
@@ -96,11 +93,7 @@ class ProductRepositoryTest{
 
     @Test
     void testDeleteProduct() {
-        Product product = new Product();
-        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        product.setProductName("Sampo Cap Bambang");
-        product.setProductQuantity(100);
-        productRepository.create(product);
+        productRepository.create(product1);
 
         productRepository.delete("eb558e9f-1c39-460e-8860-71af6af63bd6");
 
@@ -117,15 +110,10 @@ class ProductRepositoryTest{
 
     @Test
     void testFindByIdIfMoreThanOneProduct(){
-        Product product1 = new Product();
-        product1.setProductId("123");
         productRepository.create(product1);
-
-        Product product2 = new Product();
-        product2.setProductId("456");
         productRepository.create(product2);
 
-        Product found = productRepository.findById("456");
+        Product found = productRepository.findById(product2.getProductId());
 
         assertEquals(found, product2);
     }
